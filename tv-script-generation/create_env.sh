1922da986ca18d0de6fa5c4da6053bfb5f64ceba
Kaoru Fukumoto <k1fukumoto@gmail.com>
2018-01-04 18:55:06 +0900

    before downgrading tensorflow to 1.1

---
+++ tv-script-generation/create_env.sh
@@ -0,0 +1,5 @@
+conda create -n tensorflow11 python=3.5
+activate tensorflow11
+conda install pandas matplotlib jupyter notebook scipy scikit-learn
+conda install -c conda-forge tensorflow-gpu=1.1
+conda install -c conda-forge tqdm
